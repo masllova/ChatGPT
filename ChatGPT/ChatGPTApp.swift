ed62ef188cfb5a2c3ec1e0b6daaf019f2730ccbc
alexa <allexa.msv@gmail.com>
2023-05-19 14:07:40 +0300

    Initial Commit

---
+++ ChatGPT/ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTApp.swift
+//  ChatGPT
+//
+//  Created by Александра Маслова on 19.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
