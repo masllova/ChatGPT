655b2caf9f448b6caba453ba952d236caea79f7a
alexa <allexa.msv@gmail.com>
2023-05-19 14:50:01 +0300

    Connection with OpenAI is established

---
+++ ChatGPT/Objects.swift
@@ -0,0 +1,40 @@
+//
+//  Objects.swift
+//  ChatGPT
+//
+//  Created by Александра Маслова on 19.05.2023.
+//
+
+import Foundation
+
+struct MessageBody: Encodable {
+    let model: String
+    let prompt: String
+}
+
+struct ResponseBody: Decodable {
+    let id: String
+    let choices: [ResponseChoices]
+}
+
+struct ResponseChoices: Decodable {
+    let text: String
+}
+
+struct Message {
+    let id: String
+    let type: Sender
+    let text: String
+    let dateCreated: Date
+}
+
+public enum Sender {
+    case user
+    case gpt
+}
+
+enum Constants {
+    static let key = "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"
+    static let url = "https://api.openai.com/v1/completions"
+    static let model = "text-davinci-003"
+}
---
+++ ChatGPT/Service.swift
@@ -0,0 +1,35 @@
+//
+//  Service.swift
+//  ChatGPT
+//
+//  Created by Александра Маслова on 19.05.2023.
+//
+
+import Foundation
+import Alamofire
+import Combine
+
+class OpenAIService {
+    func send(message: String) -> AnyPublisher<ResponseBody, Error> {
+        let body = MessageBody(model: Constants.model, prompt: message)
+        let headers: HTTPHeaders = [
+            "Authorization": "Bearer \(Constants.key)"
+        ]
+        
+        return Future { [weak self] promise in
+            guard self != nil else {return}
+            AF.request(Constants.url, method: .post, parameters: body, encoder: .json, headers: headers)
+                .responseDecodable(of: ResponseBody.self) { response in
+                    switch response.result {
+                    case .success(let result):
+                        promise(.success(result))
+                    case .failure(let error):
+                        promise(.failure(error))
+                    }
+                }
+        }
+        .eraseToAnyPublisher()
+    }
+}
+
+
